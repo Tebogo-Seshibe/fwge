0c684e7f450d14f719880c28f2af32c71dd1ff84
Tebogo Christopher Seshibe <tseshibe@retrorabbit.co.za>
2019-03-10 22:06:36 +0200

    Oof

---
+++ src/Utility/Control.ts
@@ -0,0 +1,65 @@
+import Updateable from '../Interfaces/Updateable'
+
+export default class Control implements Updateable
+{
+    public Running: boolean = false
+    private AnimationFrame: number = -1
+
+    constructor()
+    {
+    }
+    
+    private Run(): void
+    {
+        this.AnimationFrame = window.requestAnimationFrame(this.Run)
+
+        self.Update();
+
+        if (this.Running)
+        {
+            PhysicsEngine.Update();
+            RenderEngine.Update();
+        }
+    }
+
+    public Update(): void
+    {
+        Time.Update();
+        Camera.Update();
+
+        var i = GameObject.Objects.length;
+        while (--i >= 0)
+            GameObject.Objects[i].ObjectUpdate();
+
+        Input.InputUpdate();
+    }
+        
+    Start(): void
+    {
+        if(!this.Running)
+            this.Running = true;
+
+        if (this.AnimationFrame === -1)
+            this.Run();
+    }
+    
+    Pause(): void
+    {
+        if (!this.Running)
+            this.Running = false;
+    }
+    
+    Stop(): void
+    {
+        if (this.Running)
+            this.Running = false;
+
+        if (this.AnimationFrame !== -1)
+        {
+            window.cancelAnimationFrame(this.AnimationFrame);
+            this.AnimationFrame = -1;
+        }
+
+        //Time.Reset();
+    }
+}
