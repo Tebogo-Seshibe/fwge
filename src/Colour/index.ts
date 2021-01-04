6d97a72e5785357817c2ac64f573bdc98885098f
Tebogo Christopher Seshibe <tseshibe@retrorabbit.co.za>
2021-01-04 10:42:30 +0200

    Naisu

---
+++ src/Colour/index.ts
@@ -0,0 +1,8 @@
+import Colour3 from './Colour3'
+import Colour4 from './Colour4'
+
+export default 
+{
+    Colour3,
+    Colour4
+}
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,8 @@
 import FWGE from './FWGE'
 
 export * from './Audio/Audio'
-
 export * from './Animation'
-
 export * from './Camera'
-export * from './Engine/Converter'
-export * from './Engine/Input'
-export * from './Engine/Interfaces'
-export * from './Engine/Light'
-export * from './Engine/Maths'
-export * from './Engine/Object'
-export * from './Engine/Particle System'
-export * from './Engine/Utility'
+export * from './Colour'
 
 export default FWGE
