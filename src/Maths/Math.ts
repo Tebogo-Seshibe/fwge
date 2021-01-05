f82b13c054d19a20b173870dda7c633c8ed48f84
Tebogo-Seshibe <tseshibe@gmail.com>
2021-01-05 18:10:00 +0200

    Added interpolation methods

--- src/Maths/Math.ts
+++ src/Maths/Math.ts
@@ -3,17 +3,17 @@ export const SIGNIFICANT_FIGURES: number = Math.pow(10, DECIMAL_PLACES)
 
 export const radian = (degree: number): number =>
 {
-    return Math.PI / 180 * degree
+    return clean(Math.PI / 180 * degree)
 }
 
 export const cot = (radian: number): number =>
 {
-    return 1 / Math.tan(radian)
+    return clean(1 / Math.tan(radian))
 }
 
 export const clamp = (value: number, min: number, max: number): number =>
 {
-    return Math.min(max, Math.max(min, value))
+    return clean(Math.min(max, Math.max(min, value)))
 }
 
 export const randBetween = (min: number, max: number): number =>
@@ -31,7 +31,17 @@ export const isPowerOf2 = (value: number): boolean =>
     return (value & (value - 1)) === 0
 }
 
-export const lerp = (from: number, to: number, time: number): number =>
+export const lerp = (min: number, max: number, time: number): number =>
 {
-    return from * (1 - time) + to * time
+    return clean(min + (time * (max - min)))
+}
+
+export const inverseLerp = (min: number, max: number, value: number): number =>
+{
+    return clean(value - min) / (max - min))
+}
+
+export const remap = (inputMin: number, inputMax: number, outputMin: number, outputMax: number, inputVal: number): number =>
+{
+    return clean(lerp(outputMin, outputMax, inverseLerp(inputMin, inputMax, inputVal)))
 }
